ced7ef5a145cf655c365a9bc8e46595093310309
github username <github email address>
2019-01-17 08:48:52 -0600

    tweeks

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,35 +1,44 @@
 class Api::V1::TrailsController < ApplicationController
-  before_action :find_trail, only: [:update, :show]
+  before_action :find_trail, only: [:update, :show, :destroy]
 
   def index
-    @trails = Trail.all
-    render json: @trails
+    trails = Trail.all
+    render json: trails
   end
 
   def show
-    render json: @trail, status: :accepted
+    render json: trail, status: :accepted
   end
 
-  def new
-    @trail = Trail.new
-  end
+  def create
+    current_user = User.find(params[:user_id])
 
-  def update
-    @trail.update(trail_params)
-    if @trail.save
-      render json: @trail, status: :accepted
+    trail = Trail.new(trail_params)
+    trail.users << current_user
+
+    if trail.save
+      render json: trail
     else
-      render json: { errors: @trail.error.full_messages }, status: :unprocessible_entity
+      render json: { errors: trail.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    if @trail.destroy
+    render json: @trail, status: :accepted
+    else
+      render json: { errors: trail.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
   private
 
   def trail_params
-    params.permit(:trail_number)
+    params.require(:trail).permit(:trail_number)
   end
 
   def find_trail
     @trail = Trail.find(params[:id])
   end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    @users = User.all
-    render json: @users, status: :accepted
+    users = User.all
+    render json: users, status: :accepted
   end
 
   def create
-      @user = User.create(new_user_params)
-    if @user.valid?
-      render json: @user, status: :accepted
+      user = User.create(new_user_params)
+    if user.valid?
+      render json: user, status: :accepted
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
   def login
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
-      render json: @user, status: :accepted
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      render json: user, status: :accepted
     else
       render json: { errors: 'Failed to Log In' }, status: 400
     end
